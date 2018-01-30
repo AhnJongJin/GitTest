ff67ddd942d7e47caafb39bef8f83b0d7c45b661
AhnJongJin <ajj89good@gmail.com>
2018-01-30 11:30:28 +0900

    Test

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        print("This is a Git tutorial")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
